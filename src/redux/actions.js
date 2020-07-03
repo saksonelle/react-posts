d674fe7375f009ed978a142d1c43e69033c76514
saksonelle <saksonelle@gmail.com>
2020-07-03 13:59:33 +0300

    fix: view of code actions.js page

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,29 +1,27 @@
-import { CREATE_POST, FETCH_POSTS, REQUEST_POSTS, SHOW_LOADER, HIDE_LOADER, HIDE_ALERT, SHOW_ALERT } from './types';
+import {
+  REQUEST_POSTS,
+  FETCH_POSTS,
+  CREATE_POST,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  HIDE_ALERT,
+  SHOW_ALERT
+} from './types';
 
 export function createPost(post) {
-  return {
-    type: CREATE_POST,
-    payload: post,
-  };
+  return { type: CREATE_POST, payload: post };
 }
 
 export function showLoader() {
-  return {
-    type: SHOW_LOADER,
-  };
+  return { type: SHOW_LOADER };
 }
 
 export function hideLoader() {
-  return {
-    type: HIDE_LOADER,
-  };
+  return { type: HIDE_LOADER };
 }
 
 export function showAlert(text) {
-  // return {
-  //   type: SHOW_ALERT,
-  //   payload: text,
-  // };
+  // return { type: SHOW_ALERT, payload: text };
   return (dispatch) => {
     dispatch({ type: SHOW_ALERT, payload: text });
 
@@ -34,15 +32,11 @@ export function showAlert(text) {
 }
 
 export function hideAlert() {
-  return {
-    type: HIDE_ALERT,
-  };
+  return { type: HIDE_ALERT };
 }
 
 export function fetchPosts() {
-  return {
-    type: REQUEST_POSTS,
-  };
+  return { type: REQUEST_POSTS };
   // return async (dispatch) => {
   //   try {
   //     dispatch(showLoader());
